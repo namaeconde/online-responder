d5c6052c759f57a94999f9308113d98c7497d229
Namae Conde <lunamae.conde@sensis.com.au>
2021-08-28 04:32:07 +1000

    Add routes

---
+++ web-app/src/routes.js
@@ -0,0 +1,43 @@
+import { Navigate, useRoutes } from 'react-router-dom';
+// layouts
+import DashboardLayout from './layouts/dashboard';
+import LogoOnlyLayout from './layouts/LogoOnlyLayout';
+//
+import Login from './pages/Login';
+import Register from './pages/Register';
+import DashboardApp from './pages/DashboardApp';
+import Products from './pages/Doctors';
+import Blog from './pages/RewardsHistory';
+import User from './pages/HealthHistory';
+import NotFound from './pages/Page404';
+
+// ----------------------------------------------------------------------
+
+export default function Router() {
+  return useRoutes([
+    {
+      path: '/dashboard',
+      element: <DashboardLayout />,
+      children: [
+        { path: '/', element: <Navigate to="/dashboard/app" replace /> },
+        { path: 'app', element: <DashboardApp /> },
+        { path: 'user', element: <User /> },
+        { path: 'blog', element: <Blog /> }
+      ]
+    },
+    {
+      path: '/',
+      element: <LogoOnlyLayout />,
+      children: [
+        { path: 'login', element: <Login /> },
+        { path: 'register', element: <Register /> },
+        { path: '404', element: <NotFound /> },
+        { path: '/', element: <Navigate to="/dashboard" /> },
+        { path: 'doctors', element: <Products /> },
+        { path: '*', element: <Navigate to="/404" /> }
+      ]
+    },
+
+    { path: '*', element: <Navigate to="/404" replace /> }
+  ]);
+}
