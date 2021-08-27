a5de2a597100ccf9d63c4d7516d2a0a914628ca7
Namae Conde <lunamae.conde@sensis.com.au>
2021-08-28 04:31:56 +1000

    Add find doctors dashboard

---
+++ web-app/src/components/_dashboard/doctors/index.js
@@ -0,0 +1,4 @@
+export { default as ProductFilterSidebar } from './ProductFilterSidebar';
+export { default as DoctorCard } from './DoctorCard';
+export { default as DoctorList } from './DoctorList';
+export { default as DoctorSort } from './DoctorSort';
